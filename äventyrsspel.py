bb2ed2105aef77888ba24b58cbfcd6ccf5a08b66
NorrskenetPOG <90901006+NorrskenetPOG@users.noreply.github.com>
2021-12-03 18:05:41 +0100

    Update äventyrsspel.py

--- äventyrsspel.py
+++ äventyrsspel.py
@@ -9,28 +9,28 @@ class Player():
         self.exp = 0
         self.strenght = 5
         self.inventory = []
-        self.item_bonus_list = []
+        self.item_bonus = []
 
     def player_hit(self):
         self.lifes -= 1
         
     def chest(self):
         self.inventory.append(str(rand.choice(item.item_names)))
-        self.item_bonus_list.append(int(rand.choice(item.bonus_list)))
+        self.item_bonus.append(int(rand.choice(item.bonus_range)))
     
     def show_inventory(self):
-        self.show_inventory1 = ""
-        l = zip(player.inventory, player.item_bonus_list)
+        self.inevntory_layout = ""
+        l = zip(player.inventory, player.item_bonus)
         for i, t in enumerate(l):
-            self.show_inventory1 += f"{i}. {t[0]} +{t[1]} STR\n"
-        return self.show_inventory1
+            self.inevntory_layout += f"{i}. {t[0]} +{t[1]} STR\n"
+        return self.inevntory_layout
 
-    def show_inventory_for_switch(self):
-        self.show_inventory2 = ""
-        l = zip(player.inventory[0:5], player.item_bonus_list[0:5])
+    def show_inventory_switch(self):
+        self.inevntory_layout_switch = ""
+        l = zip(player.inventory[0:5], player.item_bonus[0:5])
         for i, t in enumerate(l):
-            self.show_inventory2 += f"{i}. {t[0]} +{t[1]} STR\n"
-        return self.show_inventory2
+            self.inevntory_layout_switch += f"{i}. {t[0]} +{t[1]} STR\n"
+        return self.inevntory_layout_switch
 
     def player_level_up(self):
         self.current_level += 1
@@ -39,71 +39,78 @@ class Player():
 class Item():
     def __init__(self):
         self.item_names = ["Meme Blade", "Diamantsvärd", "Gravitationspistol", "Energisvärd", "Skorpionspjut", "BFG-9000", "Styrke-dryck", "Styrke-emblem", "Railgun", "Blad av kaos", "Mästar-svärd", "Köttbullsmacka", "Baguettespjut"]
-        self.bonus_list = range(1, 10)
+        self.bonus_range = range(1, 6)
 
 
 class Monster():
     def __init__(self):
-        self.strenghts = range(0, 23)
+        self.strenght_range = range(0, 23)
 
     def monster_types(self):
-        self.monster_strenght = rand.choice(enemies.strenghts)
+        self.monster_strenght = rand.choice(monster.strenght_range)
         self.monster_name = None
 
-        if enemies.monster_strenght < 5:
+        if monster.monster_strenght < 5:
             self.monster_name = "en Slime"
             return self.monster_name
-        elif enemies.monster_strenght < 8 and enemies.monster_strenght >= 5:
+        elif monster.monster_strenght < 8 and monster.monster_strenght >= 5:
             self.monster_name = "en Goblin"
             return self.monster_name
-        elif enemies.monster_strenght < 11 and enemies.monster_strenght >= 8:
+        elif monster.monster_strenght < 11 and monster.monster_strenght >= 8:
             self.monster_name = "ett Skelett"
             return self.monster_name
-        elif enemies.monster_strenght < 14 and enemies.monster_strenght >= 11:
+        elif monster.monster_strenght < 14 and monster.monster_strenght >= 11:
             self.monster_name = "ett Troll"
             return self.monster_name
-        elif enemies.monster_strenght < 17 and enemies.monster_strenght >= 14:
+        elif monster.monster_strenght < 17 and monster.monster_strenght >= 14:
             self.monster_name = "en Minotaur"
             return self.monster_name
-        elif enemies.monster_strenght < 20 and enemies.monster_strenght >= 17:
+        elif monster.monster_strenght < 20 and monster.monster_strenght >= 17:
             self.monster_name = "en Basilisk"
             return self.monster_name
-        elif enemies.monster_strenght < 22 and enemies.monster_strenght >= 20:
+        elif monster.monster_strenght < 22 and monster.monster_strenght >= 20:
             self.monster_name = "en Golem"
             return self.monster_name
-        elif enemies.monster_strenght <= 24 and enemies.monster_strenght >= 22:
+        elif monster.monster_strenght <= 24 and monster.monster_strenght >= 22:
             self.monster_name = "en Drake"
             return self.monster_name
 
 
 def rooms():
-    room_list = [1, 2, 3]
+    room_list = [1, 2, 3, 4, 5]
     room_randomizer = rand.choice(room_list)
 
     while True:
         clear_screen()
-        if room_randomizer == 1:
-            delay_print(f"Bakom dörren fanns en kista med en skatt")
+        if room_randomizer == 1 or room_randomizer == 2:
+            delay_print(f"Bakom dörren fanns en kista med en skatt\n")
             while True:
                 player.chest()
                 if len(player.inventory) >= 6:
-                    print(f"\nDitt inventory är fullt\n--------------------------\n{player.show_inventory_for_switch()}\n--------------------------")
-                    choice_inventory = input(f"Du måste slänga [{player.inventory[5]} +{player.item_bonus_list[5]} STR] som du hittade [S] eller byta ut det [B] -> ")
+                    print(f"\nDitt inventory är fullt\n--------------------------\n{player.show_inventory_switch()}\n--------------------------")
+                    choice_inventory = input(f"Du måste slänga [{player.inventory[5]} +{player.item_bonus[5]} STR] som du hittade [S] eller byta ut det [B] -> ")
                     if choice_inventory == "B" or choice_inventory == "b":
                         clear_screen()
-                        change_inventory = int(input(delay_print(f"\nVälj vilket vapen du vill byta ut \n--------------------------\n{player.show_inventory_for_switch()} \n[0 - 4] -> ")))
-                        clear_screen()
-                        delay_print(f"Du bytte [{player.inventory[change_inventory]} +{player.item_bonus_list[change_inventory]}] mot [{player.inventory[5]} +{player.item_bonus_list[5]} ST]")
-                        player.inventory.pop(change_inventory)
-                        player.item_bonus_list.pop(change_inventory)
-                        break
+                        delay_print(f"\nVälj vilket vapen du vill byta ut")
+                        print(f"\n--------------------------\n{player.show_inventory_switch()}")
+                        change_inventory = int(input(delay_print(f"\n[0 - 4] -> ")))
+                        if change_inventory >= 0 and change_inventory <= 4:
+                            clear_screen()
+                            delay_print(f"Du bytte [{player.inventory[change_inventory]} +{player.item_bonus[change_inventory]}] mot [{player.inventory[5]} +{player.item_bonus[5]} ST]")
+                            player.inventory.pop(change_inventory)
+                            player.item_bonus.pop(change_inventory)
+                            break
+                        else:
+                            clear_screen()
+                            continue
                     if choice_inventory == "S" or choice_inventory == "s":
                         clear_screen()
-                        delay_print(f"Du slängde [{player.inventory[5]} +{player.item_bonus_list[5]} STR]")
+                        delay_print(f"Du slängde [{player.inventory[5]} +{player.item_bonus[5]} STR]")
                         player.inventory.pop(5)
-                        player.item_bonus_list.pop(5)
+                        player.item_bonus.pop(5)
                         break
                     else:
+                        clear_screen()
                         continue
                     break
                 else:
@@ -111,22 +118,22 @@ def rooms():
                             
                         
             break
-        elif room_randomizer == 3:
+        elif room_randomizer == 3 or room_randomizer == 4:
 
-            delay_print(f"Bakom dörren fanns {enemies.monster_types()} som attackerar dig")
+            delay_print(f"Bakom dörren fanns {monster.monster_types()} som attackerar dig")
             while True:
-                if enemies.monster_strenght > player.strenght + sum(player.item_bonus_list):
+                if monster.monster_strenght > player.strenght + sum(player.item_bonus):
                     player.player_hit()
-                    delay_print(f"\nDu förlorade mot {enemies.monster_name}, -1 HP")
+                    delay_print(f"\nDu förlorade mot {monster.monster_name}, -1 HP\n")
                     break
                 else:
-                    delay_print(f"\nDu besegrade {enemies.monster_name}, +1 LVL")
+                    delay_print(f"\nDu besegrade {monster.monster_name}, +1 LVL\n")
                     player.player_level_up()
                     break
             break
                 
         else:
-            delay_print("Du tog skada av en fälla, -1 HP")
+            delay_print("Du tog skada av en fälla, -1 HP\n")
             player.player_hit()
             break
 
@@ -137,24 +144,26 @@ def delay_print(meningar):
     for tecken in meningar:
         sys.stdout.write(tecken)
         sys.stdout.flush()
-        time.sleep(0.1)
+        time.sleep(0.035)
     return ""
 
 player = Player(10)
 item = Item()
-enemies = Monster()
+monster = Monster()
 max_lifes = 10
 
+print("\033[1;32;40m")
+clear_screen()
 character_name = str(input(delay_print("\nVad heter din karaktär?\nSkriv in för att starta -> ")))
 clear_screen()
 
 while True:
-    print("\n----------------------------------\nDu är i ett rum med tre dörrar...\n\n    Se spelarinfo              [S]\n    Öppna inventory            [I]\n    Gå genom vänster dörr      [V]\n    Gå genom mitten dörren     [M]\n    Gå genom höger dörr        [H]\n----------------------------------")
+    print("\n--------------------------------------------\nDu har kommit till ett rum med tre dörrar...\n\n    Se spelarinfo                    [S]\n    Öppna inventory                  [I]\n    Gå genom vänster dörr            [V]\n    Gå genom mitten dörren           [M]\n    Gå genom höger dörr              [H]\n--------------------------------------------")
     choice = input(delay_print("Gör ett val -> "))
     
     if choice == "s" or choice == "S":
         clear_screen()
-        print(f"\n----------------\n{character_name}    LVL.{player.current_level}\n\nHP:  [{player.lifes}/{max_lifes}]\nSTR: [{player.strenght + sum(player.item_bonus_list)}]\n----------------")
+        print(f"\n----------------\n{character_name}    LVL.{player.current_level}\n\nHP:  [{player.lifes}/{max_lifes}]\nSTR: [{player.strenght + sum(player.item_bonus)}]\n----------------")
         
     elif choice == "i" or choice == "I":
         clear_screen()
@@ -168,6 +177,10 @@ while True:
         
     elif choice == "h" or choice == "H":
         rooms()
+    
+    else:
+        clear_screen()
+        continue
 
     if player.current_level == player.max_level:
         print("Du har vunnit spelet")
