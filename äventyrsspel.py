7ab04e8042e085dd732f7d8ea8d620c677628d37
NorrskenetPOG <90901006+NorrskenetPOG@users.noreply.github.com>
2022-01-14 11:04:01 +0100

    Update äventyrsspel.py

--- äventyrsspel.py
+++ äventyrsspel.py
@@ -3,22 +3,29 @@ from os import system
 
 class Player():
     def __init__(self, lifes):
+        """
+        Funktionen initsierar klassen Player's atributer och startvärden
+        """
         self.lifes = lifes
         self.max_level = 10
         self.current_level = 0
-        self.exp = 0
+        self.max_xp = 5
+        self.xp = 0
         self.strenght = 5
         self.inventory = []
         self.item_bonus = []
 
     def player_hit(self):
-        self.lifes -= 1
-        
-    def chest(self):
-        self.inventory.append(str(rand.choice(item.item_names)))
-        self.item_bonus.append(int(rand.choice(item.bonus_range)))
+        '''
+        Funktion som minskar livet på avataren när hen blir träffad.
+        '''
+        self.lifes -= 0.5
     
     def show_inventory(self):
+        """
+        Denna funktion tar två listor och med hjälp av funtionen Zip läggs dessa ihop. T.ex. [a, b, c] och [1, 2, 3] blir [1a, 2b, 3c].
+        Sedan printer den listans index var för sig för att ge ett snyggt textbaserat gränssnitt.
+        """
         self.inventory_layout = ""
         lists = zip(player.inventory[0:5], player.item_bonus[0:5])
         for index, list_content in enumerate(lists):
@@ -26,18 +33,46 @@ class Player():
         return self.inventory_layout
 
     def player_level_up(self):
-        self.current_level += 1
+        '''
+        Funktion lägger till xp till karaktären och när karaktären når max xp för en nivå ökar nivån och gränsen för att ranka upp (xp_max).
+        '''
+        self.xp += 5
+        
+        if self.xp >= player.max_xp:
+            print(f"Du levlade upp, LVL.{player.current_level + 1}")
+            self.current_level += 1
+            self.xp = 0
+            self.max_xp += 2
+            return self.current_level and self.xp and self.max_xp
 
 class Item():
+    """
+    Funktionen initsierar klassen Item's atributer och startvärden.
+    """
     def __init__(self):
         self.item_names = ["Meme Blade", "Diamondsword", "Gravitygun", "Energisvärd", "Pizzaskärare", "BFG-9000", "Styrke-dryck", "Styrke-emblem", "Railgun", "Blades of kaos", "Matersword", "Köttbullsmacka", "Baguettespjut"]
         self.bonus_range = range(1, 6)
 
+    def chest(self):
+        '''
+        Funktion som lägger in ett vanligt item plus ett bonus item när avataren hittar en kista.
+        '''
+        player.inventory.append(str(rand.choice(item.item_names)))
+        player.item_bonus.append(int(rand.choice(item.bonus_range)))
+
 class Monster():
     def __init__(self):
+        '''
+        Funktionen initsierar klassen Monster's atributer och startvärden.
+        '''
         self.strenght_range = range(0, 23)
 
     def monster_types(self):
+        """
+        Denna funktion ska slumpmässigt välja ett monster. 
+        Den gör det genom att först välja en också slumpmässad styrka.
+        Alla monster är rangordnade efter styrka och baserat på den slumpade styrkan väljer den ett monster.
+        """
         self.monster_strenght = rand.choice(monster.strenght_range)
         self.monster_name = None
 
@@ -73,12 +108,14 @@ def rooms():
     while True:
         clear_screen()
         if room_randomizer == 1 or room_randomizer == 2:
+            print("\033[1;33;40m")
             delay_print(f"Bakom dörren fanns en kista med en skatt\n")
             while True:
-                player.chest()
+                item.chest()
                 if len(player.inventory) >= 6:
-                    print(f"Ditt inventory är fullt\n\n--------------------------\n{player.show_inventory()}")
-                    choice_inventory = input(f"\nDu måste slänga [{player.inventory[5]} +{player.item_bonus[5]} STR] som du hittade [S] eller byta ut det [B] -> ")
+                    print("\033[1;37;40m")
+                    print(f"\nDitt inventory är fullt\n\n--------------------------\n{player.show_inventory()}")
+                    choice_inventory = input(f"\nVälj för [{player.inventory[5]} +{player.item_bonus[5]} STR] att [S] slänga eller [B] byta -> ")
                     if choice_inventory == "B" or choice_inventory == "b":
                         while True:
                             clear_screen()
@@ -110,27 +147,37 @@ def rooms():
             break
 
         elif room_randomizer == 3 or room_randomizer == 4:
-            delay_print(f"Bakom dörren fanns {monster.monster_types()} som attackerar dig")
+            delay_print(f"\nBakom dörren fanns {monster.monster_types()} som attackerar dig")
             while True:
                 if monster.monster_strenght > player.strenght + sum(player.item_bonus):
+                    print("\033[1;91;40m")
                     player.player_hit()
-                    delay_print(f"\nDu förlorade mot {monster.monster_name}, -1 HP\n")
+                    delay_print(f"Du förlorade mot {monster.monster_name}, -0.5 HP\n")
                     break
                 else:
-                    delay_print(f"\nDu besegrade {monster.monster_name}, +1 LVL\n")
+                    print("\033[1;92;40m")
+                    delay_print(f"Du besegrade {monster.monster_name}, +6 XP\n")
                     player.player_level_up()
                     break
             break
 
         else:
-            delay_print("Du tog skada av en fälla, -1 HP\n")
+            delay_print(f"\nBakom dörren fanns fälla")
+            print("\033[1;91;40m")
+            delay_print("Du tog skada av en fälla, -0.5 HP\n")
             player.player_hit()
             break
 
 def clear_screen():
+    ''' 
+    Winows cls kommando för att rensa skärmen.
+    '''
     system("cls || clear")
 
 def delay_print(meningar):
+    """
+    Fördröjer printar så att det som inte printas ut, utan skrivs ut.
+    """
     for tecken in meningar:
         sys.stdout.write(tecken)
         sys.stdout.flush()
@@ -142,18 +189,35 @@ item = Item()
 monster = Monster()
 max_lifes = 10
 
-print("\033[1;32;40m")
+print("\033[1;37;40m")
+"""
+ANSI escape-kod som sätter texten till en färg;
+\033[ = Escape-kod, alltid samma
+0 = Stil, 0 för normalt
+36 = Text färg, 36 för mörk grått
+40m = Bakgrundsfärg, 40 för svart
+
+Förekomer flera gånger i koden för att ändra färger
+"""
+
+#Huvudprogram
 clear_screen()
 character_name = str(input(delay_print("Vad heter din karaktär?\nSkriv in för att starta -> ")))
 clear_screen()
 
 while True:
+    '''
+    Här är start menyn. Den loopar så länge avataren inte uppnår max level eller har noll liv. 
+    Här kan spelaren välja ifall hen vill se information såsom namn, level, liv och styrka.
+    Hen kan även öppna sitt inventory härifrån, och välja på tre dörrar som alla gör samma sak, öppnar rooms funtkionen.
+    '''
+    print("\033[1;37;40m")
     print("\n--------------------------------------------\nDu har kommit till ett rum med tre dörrar...\n\n    Se spelarinfo                    [S]\n    Öppna inventory                  [I]\n    Gå genom vänster dörr            [V]\n    Gå genom mitten dörren           [M]\n    Gå genom höger dörr              [H]\n--------------------------------------------")
     choice = input(delay_print("Gör ett val -> ")).lower()
     
     if choice == "s":
         clear_screen()
-        print(f"----------------\n{character_name}    LVL.{player.current_level}\n\nHP:  [{player.lifes}/{max_lifes}]\nSTR: [{player.strenght + sum(player.item_bonus)}]\n----------------")
+        print(f"----------------------------\n{character_name}    LVL.{player.current_level} ({player.xp}/{player.max_xp} XP)\n\nHP:  [{player.lifes}/{max_lifes}]\nSTR: [{player.strenght + sum(player.item_bonus)}]\n----------------------------")
         
     elif choice == "i":
         clear_screen()
